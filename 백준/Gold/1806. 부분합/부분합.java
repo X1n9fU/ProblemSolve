7a515cc7eff1e639c20c625132ccd0527d49e515
민국 <121088189+X1n9fU@users.noreply.github.com>
2024-04-09 13:11:06 +0900

    [Gold IV] Title: 부분합, Time: 288 ms, Memory: 23868 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,30 +1,32 @@
+
 import java.util.*;
- 
+import java.io.*;
+
 public class Main {
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        int n = scan.nextInt();
-        int s = scan.nextInt();
-        
-        int[] nums = new int[n + 1];
-        for(int i = 0; i < n; i++) {
-            nums[i] = scan.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken()); //수열 길이 입력
+        int S = Integer.parseInt(st.nextToken()); //합 출력기준
+        int res = Integer.MAX_VALUE;
+
+        int[] arr = new int[N + 1]; //입력받을 수열을 저장하는 배열
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()); //수열을 이루는 수 입력
         }
+
+        int start = 0, end = 0, sum = 0; //길이를 구하기 위한 start, end 변수선언, sum은 start~end까지의 합
         
-        int min = Integer.MAX_VALUE;
-        int start = 0;
-        int end = 0;
-        int total = 0;
-        while(start <= n && end <= n) {
-            if(total >= s && min > end - start) min = end - start;
-            
-            if(total < s) total += nums[end++];
-            else total -= nums[start++];
+        while(start <= N && end <= N) {
+            if(sum >= S && res > end - start) res = end - start;
+
+            if(sum < S) sum += arr[end++];
+            else sum -= arr[start++];
         }
-        
-        if(min == Integer.MAX_VALUE) System.out.println("0");
-        else System.out.println(min);
+
+        if (res == Integer.MAX_VALUE) System.out.println(0); //res값이 업데이트 되어 있지 않다면 0출력 == 합을 만드는 것이 불가능함을 의미
+        else System.out.println(res); //이외에 경우 업데이트 한 res값 출력
     }
-}    
+}
