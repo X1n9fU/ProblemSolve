0847751f9b98d6d788b4949397b01d451d4fd3fa
Min Kyung Kim <minkyeong0244@gmail.com>
2025-07-10 20:51:00 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 5.57 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        Set<Integer> nums = new HashSet<>();
+        
+        for (int i=0; i<numbers.length-1; i++) {
+            for (int j=i+1; j<numbers.length; j++) {
+                nums.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return nums.stream()
+                .mapToInt(Integer::intValue)
+                .sorted().toArray();
+    }
+}
