f133e48f26fc07c22af03b26254369a9800737ae
Min Kyung Kim <minkyeong0244@gmail.com>
2025-07-11 16:52:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 91.30 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,33 +2,16 @@ import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> numOfCompletion = new HashMap<>();
-        
-        for (int i=0; i<completion.length; i++) {
-            String name = completion[i];
-            int num = 1;
-            if (numOfCompletion.containsKey(name)){
-                num = numOfCompletion.get(name) + 1;
-            }
-            numOfCompletion.put(name, num);
-        }
-        
-        String result = "";
-        for (int i=0; i<participant.length; i++) {
-            String name = participant[i];
-            if (numOfCompletion.containsKey(name)) {
-                int num = numOfCompletion.get(name) - 1;
-                if (num == 0) { //모든 참여자 확인 
-                    numOfCompletion.remove(name);
-                } else { //아직 동명이인 존재
-                    numOfCompletion.put(name, num);
-                }
-            } else {
-                result = name;
-                break;
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
             }
         }
-        
-        return result;
+        return answer;
     }
 }
