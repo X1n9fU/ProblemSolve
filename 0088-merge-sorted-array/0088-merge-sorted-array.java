7b7cc5d14510a3e26a0fd4fea91f54334adf9707
Min Kyung Kim <minkyeong0244@gmail.com>
2025-07-11 15:58:19 +0900

    Time: 0 ms (100%), Space: 42.8 MB (8.37%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -11,65 +11,37 @@ class Solution {
             return;
         }
 
-        for (int i=0; i<n; i++){
-            nums1[m+i] = nums2[i];
+        //새로운 배열에 merge sort로 정렬
+
+        int [] result = new int[m+n];
+        int one = 0;
+        int two = 0;
+        int index=0;
+        while (one < m && two < n) {
+            if (nums1[one] <= nums2[two]){
+                result[index] = nums1[one++];
+            } else {
+                result[index] = nums2[two++];
+            }
+            index++;
         }
 
-        Arrays.sort(nums1);
+        if (one == m) {
+            int end = n-two;
+            for (int i=0; i<end; i++) {
+                result[index+i] = nums2[two+i];
+            }
+        }
+
+        if (two == n) {
+            int end = m-one;
+            for (int i=0; i<end; i++) {
+                result[index+i] = nums1[one+i];
+            }
+        }
+
+        for (int i=0; i<m+n; i++){
+            nums1[i] = result[i];
+        }
     }
-
-
-    //     int indexOfNums1 = 0;
-    //     int indexOfNums2 = 0;
-    //     /*
-    //     num1과 num2를 차례대로 비교
-    //     만약 num2가 더 작다면 비교한 것에 넣고,
-    //     넣을 곳 뒤에 숫자들(m - 확인한 인덱스)은 그만큼 밀림
-    //     만약 num1를 다 확인했는데 num2가 남았다면 그대로 순차대로 num1에 덮어쓰기
-    //     */
-
-    //     while (indexOfNums1 < m+n) {
-    //         if (nums1[indexOfNums1] == 0 && indexOfNums > m) {
-    //             break;
-    //         }
-    //         if (nums1[indexOfNums1] > nums2[indexOfNums2]) { 
-    //             for (int i=0; i<m-indexOfNums1; i++){
-    //                 nums1[indexOfNums1+1] = temp;
-    //                 temp = nums1[indexOfNums1+1];
-    //                 nums1[indexOfNums1+1] = nums1[indexOfNums1];
-    //             }
-    //             nums1[indexOfNums1] = nums2[indexOfNums2];
-    //             indexOfNums2++;
-    //         }
-    //         indexOfNums1++;
-    //     }
-
-
-    //     int temp = 0;
-    //     boolean pass = false;
-    //     while (indexOfNums1 < m+n) {
-    //         if (nums1[indexOfNums1] == 0 && indexOfNums1 >= m || indexOfNums2 >= n) {
-    //             break;
-    //         }
-    //         if (nums1[indexOfNums1] <= nums2[indexOfNums2]) {
-    //             indexOfNums1 ++;
-    //         } else {
-    //             for (int i=0; i<m+n-indexOfNums1-1; i++){
-    //                 temp = nums1[indexOfNums1+1+i];
-    //                 nums1[indexOfNums1+1+i] = nums1[indexOfNums1+i];
-    //             }
-    //             nums1[indexOfNums1] = nums2[indexOfNums2];                
-    //             indexOfNums1++;
-    //             indexOfNums2++;
-    //         }
-    //     }
-
-    //     if (indexOfNums2 < n){
-    //         for (int i=0; i<n-indexOfNums2; i++) {
-    //             System.out.println(indexOfNums1);
-    //             System.out.println(indexOfNums2);
-    //             nums1[indexOfNums1+i] = nums2[indexOfNums2+i];
-    //         }
-    //     }
-    // }
 }
