92769c1079aa7279ab0cfc53f2da1fef3aa0a7dd
Min Kyung Kim <minkyeong0244@gmail.com>
2025-07-11 16:04:26 +0900

    Time: 133 ms (2.51%), Space: 42.5 MB (26.93%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,36 +1,9 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //새로운 배열에 merge sort로 정렬
-
-        int [] result = new int[m+n];
-        int one = 0;
-        int two = 0;
-        int index=0;
-        while (one < m && two < n) {
-            if (nums1[one] <= nums2[two]){
-                result[index] = nums1[one++];
-            } else {
-                result[index] = nums2[two++];
-            }
-            index++;
+        for (int i=0; i<n; i++){
+            nums1[m+i] = nums2[i];
         }
 
-        if (one == m) {
-            int end = n-two;
-            for (int i=0; i<end; i++) {
-                result[index+i] = nums2[two+i];
-            }
-        }
-
-        if (two == n) {
-            int end = m-one;
-            for (int i=0; i<end; i++) {
-                result[index+i] = nums1[one+i];
-            }
-        }
-
-        for (int i=0; i<m+n; i++){
-            nums1[i] = result[i];
-        }
+        Arrays.sort(nums1);
     }
 }
