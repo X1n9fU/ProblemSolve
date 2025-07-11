a7c01567d150f11b52f7622cfc96b0b7a3268c58
Min Kyung Kim <minkyeong0244@gmail.com>
2025-07-11 16:04:39 +0900

    Time: 131 ms (2.51%), Space: 42 MB (89.62%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,5 +1,16 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        if (m == 0) {
+            for (int i=0; i<nums2.length; i++){
+                nums1[i] = nums2[i];
+            }
+            return;
+        }
+        if (n == 0) {
+            return;
+        }
+
         for (int i=0; i<n; i++){
             nums1[m+i] = nums2[i];
         }
