cc850fff8d883595834d749e72dce19d1cc3934b
Min Kyung Kim <minkyeong0244@gmail.com>
2025-07-11 17:16:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.09 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,15 +1,18 @@
-import java.util.*;
+import java.util.HashMap;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        int i;
-        for ( i=0; i<completion.length; i++){
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
 
-            if (!participant[i].equals(completion[i])){
-                return participant[i];
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
+                break;
             }
         }
-        return participant[i];
+        return answer;
     }
 }
