959519486d1a65b08233bd446d2d878be5504888
Min Kyung Kim <minkyeong0244@gmail.com>
2025-07-12 22:50:53 +0900

    Time: 46 ms (16.6%), Space: 45 MB (63.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int [] answer = {};
         for (int i=1; i<nums.length; i++) { // 현재 확인하는 인덱스에서 i만큼 전에 위치한 값
             for (int j=i; j<nums.length; j++) { // 현재 확인하는 인덱스
                 if (nums[j] + nums[j-i] == target) {
-                    return new int [] {j-i, j};
+                    answer = new int [] {j-i, j};
+                    break;
                 }
             }
         }
-        return null;
+        return answer;
     }
 }
