7689df659032f670e275ff078fc9710d9883fd87
Min Kyung Kim <minkyeong0244@gmail.com>
2025-07-12 22:51:08 +0900

    Time: 4 ms (57.24%), Space: 44.8 MB (78.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,21 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
         int [] answer = {};
-        for (int i=1; i<nums.length; i++) { // 현재 확인하는 인덱스에서 i만큼 전에 위치한 값
-            for (int j=i; j<nums.length; j++) { // 현재 확인하는 인덱스
-                if (nums[j] + nums[j-i] == target) {
-                    answer = new int [] {j-i, j};
-                    break;
-                }
+        
+        //HashMap에 넣는 과정
+        for (int i=0; i<nums.length; i++) {
+            hm.put(nums[i], i);
+        }
+		
+        
+        for (int i=0; i<nums.length; i++) {
+            int minus = target - nums[i]; // x를 구하는 과정
+            if (hm.containsKey(minus)) { //y를 찾는 과정
+                int index = hm.get(minus);
+                if (index == i) continue; // 동일한 인덱스는 포함하지 않는다.
+                answer = i < index ? new int[] {i, index} : new int[] {index, i}; //인덱스를 올바르게 정렬
+                break;
             }
         }
         return answer;
