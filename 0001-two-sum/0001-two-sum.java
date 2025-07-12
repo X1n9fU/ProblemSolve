8f267b76dbfce5de2ef029a1d5470874be3b9140
Min Kyung Kim <minkyeong0244@gmail.com>
2025-07-12 22:25:31 +0900

    Time: 2 ms (98.97%), Space: 45.4 MB (10.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,9 +4,9 @@ class Solution {
         for (int i=0; i<nums.length; i++) {
             int minus = target - nums[i];
             if (hm.containsKey(minus)) {
-                int index = hm.get(minus);
-                if (index == i) continue;
-                return i < index ? new int[] {i, index} : new int[] {index, i};
+                // int index = hm.get(minus);
+                // return i < index ? new int[] {i, index} : new int[] {index, i};
+                return new int[] {hm.get(minus), i};
             } else hm.put(nums[i], i);
         }
         return null;
