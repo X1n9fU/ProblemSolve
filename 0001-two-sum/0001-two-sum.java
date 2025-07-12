10a4704f5d389baa592633e6152f130ebc30e8d8
Min Kyung Kim <minkyeong0244@gmail.com>
2025-07-12 22:34:09 +0900

    Time: 2 ms (98.97%), Space: 45 MB (63.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> hm = new HashMap<>();
+        int [] answer = {};
         for (int i=0; i<nums.length; i++) {
             int minus = target - nums[i];
             if (hm.containsKey(minus)) {
-                return new int[] {i, hm.get(minus)};
+                answer = new int[] {i, hm.get(minus)};
+                break;
             } else hm.put(nums[i], i);
         }
-        return null;
+        return answer; //출력값이 꼭 있기 때문에 null 처리
     }
 }
