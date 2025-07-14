b79fcbabb82b4370da4d194484a1157eda1e31e7
Min Kyung Kim <minkyeong0244@gmail.com>
2025-07-14 14:25:42 +0900

    Time: 0 ms (100%), Space: 41.6 MB (92.24%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,26 +3,21 @@ import java.util.*;
 
 class Solution {
     public boolean isValid(String s) {
-        Stack<Integer> st = new Stack<>();
-
-        for (int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            if (c=='(') st.push(1);
-            else if (c=='{') st.push(2);
-            else if (c=='[') st.push(3);
-            else {
-                if (!st.isEmpty()) {
-                    int close = st.pop();
-                    if (c==')' && close != 1) return false;
-                    else if (c == '}' && close != 2 ) return false;
-                    else if (c==']' && close != 3) return false;
-                } else return false;
+        char [] stack = new char[s.length()];
+        int index = -1;
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(' -> stack[++index] = ')';
+                case '{' -> stack[++index] = '}';
+                case '[' -> stack[++index] = ']';
+                default -> {
+                    if (index == -1 || stack[index] != c) return false;
+                    index--;
+                }
             }
         }
-        if (!st.isEmpty()) return false;
-
+        if (index != -1) return false;
         return true;
-    
     }
 
 }
