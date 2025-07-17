9b472c1f274678e6bb92b7884f9b8840235a81b1
Min Kyung Kim <minkyeong0244@gmail.com>
2025-07-17 15:51:51 +0900

    Time: 0 ms (100%), Space: 45.5 MB (89.62%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,11 +1,9 @@
 class Solution {
-    static int answer = -1;
     public int search(int[] nums, int target) {
         int l=0; int r=nums.length-1;
 
         while (l <= r){
             int middle = (r-l)/2 + l;
-            System.out.println(middle);
             if (nums[middle] == target) return middle;
             else if (nums[middle] > target) r=middle-1;
             else if (nums[middle] < target) l=middle+1;
